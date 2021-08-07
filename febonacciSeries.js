6235b162c9349c79cb14ee54303b4b4aa49aa97b
Mohammad Mostafa <mohammad-mostafa94>
2021-08-07 12:33:21 +0600

    fibonacci two types system added

--- febonacciSeries.js
+++ febonacciSeries.js
@@ -1,28 +1,18 @@
-let first = 0;
-let second = 1;
-let n = 5;
-for (let count = 0; count < n; count++) {
-    if (count <= 1) {
-        var fibo = count;
-    }
-    else {
-        fibo = first + second;
-        
-        first = second;
-    }
-}
-
-// let first =  0;
-// let second = 1;
-// let n = 5;
-// for (let count = 0; count < n; count++) {
-//     if (count<=1) {
-//         var fibo = count;
+// let first = 0, second = 1, fibo, numbers = 6;
+// for (let count = 0; count < numbers; count++) {
+//     if (count < 2) {
+//         fibo = count;
 //     }
-//     else{
+//     else {
 //         fibo = first + second;
 //         first = second;
 //         second = fibo;
 //     }
 //     console.log(fibo);
 // }
+
+let fibo = [0,1];
+for (let index = 2; index < 7 ; index++) {
+    fibo[index] = fibo[index - 1] + fibo[index - 2];
+    console.log(fibo[index]);
+}
